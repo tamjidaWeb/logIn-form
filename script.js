fd87b4120ceca6e2c2982aa0f213f752e7f69d16
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-28 14:26:59 +0600

    commited

---
+++ script.js
@@ -0,0 +1,55 @@
+let signinBtn = document.getElementById('signinBtn');
+let signupBtn = document.getElementById('signupBtn');
+
+let nameField = document.getElementById('nameField');
+let title = document.getElementById('title');
+
+
+
+
+signinBtn.onclick = function(){
+    nameField.style.maxHeight = '0';
+    title.innerHTML = 'Sign In';
+    signupBtn.classList.add('disable');
+    signinBtn.classList.remove('disable');
+}
+signupBtn.onclick = function(){
+    nameField.style.maxHeight = '60px';
+    title.innerHTML = 'Sign Up';
+    signinBtn.classList.add('disable');
+    signupBtn.classList.remove('disable');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let nameField = document.getElementById('nameField');
+// let title = document.getElementById('title');
+// function signUpBtn(){
+//     let signupBtn = document.getElementById('signupBtn');
+//     let signinBtn = document.getElementById('signinBtn');
+//     nameField.style.maxHeight = '60px';
+//     title.innerHTML = 'Sign Up';
+//     signinBtn.classList.add('disable');
+//     signupBtn.classList.remove('disable');
+// }
+
+// function signInBtn(){
+//     let signinBtn = document.getElementById('signinBtn');
+//     let signupBtn = document.getElementById('signupBtn');
+//     nameField.style.maxHeight = '0px';
+//     title.innerHTML = 'Sign In';
+//     signupBtn.classList.add('disable');
+//     signinBtn.classList.remove('disable');
+// }
